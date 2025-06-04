3522d9634666c9d22947cb2e13d828fbefb73ea5
Rodrigo Ceccato <29164832+rodrigo-ceccato@users.noreply.github.com>
2025-06-04 11:21:37 -0300

    Add CI workflow for SIF img and python check (#2)
    - Add CI workflow to cache SIF and validate Python
    
    - Reduce disk usage in CI

--- sifs/get_image.sh
+++ sifs/get_image.sh
@@ -1,5 +1,16 @@
 # this is an apptainer that I froze to build llvm
 # for some reason this is not working on sorgan
-mkdir -p $(realpath .cache)
-chmod -R u+rwx $(realpath .cache)
-APPTAINER_NOHTTPS=1 APPTAINER_CACHEDIR=$(realpath .cache) apptainer pull docker://rodrigoceccato/ompc-base:latest
+set -e
+
+# allow the caller to override cache directories
+CACHE_DIR="${APPTAINER_CACHEDIR:-$(realpath .cache)}"
+TMP_DIR="${APPTAINER_TMPDIR:-$CACHE_DIR/tmp}"
+
+mkdir -p "$TMP_DIR"
+chmod -R u+rwx "$CACHE_DIR"
+
+APPTAINER_NOHTTPS=1 \
+APPTAINER_CACHEDIR="$CACHE_DIR" \
+APPTAINER_TMPDIR="$TMP_DIR" \
+SINGULARITY_CACHEDIR="$CACHE_DIR" \
+apptainer pull --disable-cache --force docker://rodrigoceccato/ompc-base:latest
