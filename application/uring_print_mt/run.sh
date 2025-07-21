5ffadd7184ef3471ed07501e6b99030a42e8c0d8
rodrigo-ceccato <rodrigoceccatodefreitas@gmail.com>
2025-07-21 18:02:17 +0000

    io_uring config test

--- application/uring_print_mt/run.sh
+++ application/uring_print_mt/run.sh
@@ -6,13 +6,17 @@ clang --version
 cd "$(dirname "$0")"
 #rm -fr build
 mkdir -p build
+export CC=/home/cc/gpu-libc-artifacts/llvm-infra/llvm-installs/apptainer-Debug/bin/clang
+export CXX=/home/cc/gpu-libc-artifacts/llvm-infra/llvm
+
+cmake -S . -B build \
+ -DCMAKE_PREFIX_PATH=/home/cc/gpu-libc-artifacts/llvm-infra/llvm-installs/apptainer-Debug \
+
 
 cd build
-cmake ..
-# cmake --build . -- VERBOSE=1
-#cmake --build . -- VERBOSE=1
+cmake --build . -- VERBOSE=1
 
 echo "--------------"
 ./demo_cpu
 echo "--------------"
-#./demo_target
+./demo_target
