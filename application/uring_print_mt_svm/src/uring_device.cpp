a1f5f963e278a012df310856375cae367adfa0f7
rodrigo-ceccato <rodrigoceccatodefreitas@gmail.com>
2025-07-31 13:28:36 +0000

    port fixes to polling implementation

--- application/uring_print_mt_svm/src/uring_device.cpp
+++ application/uring_print_mt_svm/src/uring_device.cpp
@@ -9,13 +9,15 @@ void uring_fn(void *ptr)
 {
   int is_initial_device = omp_is_initial_device();
   assert(!is_initial_device && "NOT ON DEVICE");
-  printf("kernel launched!\n");
+  // printf("kernel launched!\n");
   uring_ctx_t *ctx = (uring_ctx_t *)ptr;
-  printf("device sq_ring_ptr=%p sqes=%p\n", ctx->sq_ring_ptr, ctx->sqes_dev);
+  // printf("device sq_ring_ptr=%p sqes=%p\n", ctx->sq_ring_ptr, ctx->sqes_dev);
   uring_perror((uring_ctx_t *)ptr,
-                  "Hello from the device!\n", 24);
+                  "Hello from the device!\n\0", 24);
+  uring_perror((uring_ctx_t *)ptr,
+                  "Hello again from the device!\n\0", 34);
 
-  printf("Done!\n");
+  // printf("Done!\n");
 
 }
 #pragma omp end declare target
